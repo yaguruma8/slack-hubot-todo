7de96bac5b0d073ebc50e7a5eed9bbecd9fff354
yaguruma <yaguruma_y@yahoo.co.jp>
2021-03-16 17:37:32 +0900

    botのドキュメントを記述

--- todo-bot/scripts/todo.js
+++ todo-bot/scripts/todo.js
@@ -1,4 +1,18 @@
+// Description:
+//     TODO を管理する Slack bot です
+// Commands:
+//     hubot add - TODO を作成する
+//     hubot done - TODO を完了にする
+//     hubot del - TODO を削除する
+//     hubot list - 未完了の TODO の一覧表示
+//     hubot donelist - 完了済の TODO の一覧表示
+
 'use strict';
+
 const todo = require('todo');
-console.log(todo.list());
-module.exports = robot => {};
+
+module.exports = (robot) => {
+    robot.respond(/hello (.+)/i, (msg) => {
+        msg.send(`Hello, World ${msg.match[1].trim()}!`);
+    });
+};
